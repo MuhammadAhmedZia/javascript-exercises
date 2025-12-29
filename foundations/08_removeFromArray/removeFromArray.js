33c0d6ecbda9ea07076aa0b023b1a6e7c0c7ae9d
Muhammad Ahmed Zia <umerdinahmedzia@gmail.com>
2025-12-29 13:51:47 +0500

    Complete the problem of 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
 
 const removeFromArray = function(arr, ...itemsToRemove) {
+//    simple method
     let newArray = [];
     arr.forEach(el => {
         if(!itemsToRemove.includes(el)){
@@ -7,14 +8,10 @@ const removeFromArray = function(arr, ...itemsToRemove) {
         }
     });
     return newArray;
+
+    // advance method
     // return arr.filter(items  => !itemsToRemove.includes(items));
 };
-
-
-// const removeFromArray = function(arr, ...itemsToRemove) {
-//     // Advance Method
-//     return arr.filter(items  => !itemsToRemove.includes(items));
-// };
 
 let result = removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
  result = removeFromArray(["apple", "banana", 4,"cherry"],"banana",4)
