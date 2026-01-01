138c94dc3cf5ca7b3f03eb4959ad3308b95fc7ab
Muhammad Ahmed Zia <umerdinahmedzia@gmail.com>
2026-01-01 22:41:08 +0500

    Complete the foundation excercise 11_temConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  const celcius = (fahrenheit - 32) * 5 / 9;
+  return Math.round(celcius * 10)/ 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celcius) {
+  const fahrenheit = (celcius * 9 / 5) + 32;
+  return Math.round(fahrenheit * 10)/ 10;
 };
+
+console.log(convertToCelsius(32));
+console.log(convertToFahrenheit(0));
+
 
 // Do not edit below this line
 module.exports = {
