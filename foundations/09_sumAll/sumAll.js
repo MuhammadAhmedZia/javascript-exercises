dee89ba283c47c34cbbb511afac058c911434d85
Muhammad Ahmed Zia <umerdinahmedzia@gmail.com>
2026-01-01 22:12:21 +0500

    Complete the foundation exercise of 09_sumAll and 10_leapYears

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if(!Number.isInteger(min) || Number.isInteger(max)) return "Error"
+    if (min < 0 || max < 0) return "Error"
 
+    let sum = 0;
+    let start = Math.min(min, max);
+    let end = Math.max(min, max);
+    for (let i= start; i <= end; i++) {
+        sum += i;
+    }
+    
+    return sum;
 };
+console.log(sumAll(1,4));
+console.log(sumAll(10,4));
+console.log(sumAll(1,-4));
+
+// let a =10;
+// let b = 1;
+// s = 0;
+// for (let i = a; i <= b; i++) {
+//     s += i;
+//     console.log(s);
+    
+// }
+
+// console.log(s);
+
 
 // Do not edit below this line
-module.exports = sumAll;
+// module.exports = sumAll;
--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+const leapYears = function (year) {
 
-};
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 100 == 0 && year % 400 == 0)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(leapYears(2000));
+console.log(leapYears(2004));
+console.log(leapYears(1997));
+console.log(leapYears(2024));
+console.log(leapYears(2025));
+console.log(leapYears(2016));
+
 
 // Do not edit below this line
 module.exports = leapYears;
